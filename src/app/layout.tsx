4796954ec19124e905f97c1d7fcbee8fe98c26ea
aishwarya more <aishsony88@gmail.com>
2025-02-15 15:09:18 +0530

    bug fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import ScrollToTop from "@/components/ScrollToTop";
 import { Roboto, Raleway } from "next/font/google"; // Import both fonts
-import "node_modules/react-modal-video/css/modal-video.css";
 import "../styles/index.css";
 // import 'primeflex/primeflex.css';
 
