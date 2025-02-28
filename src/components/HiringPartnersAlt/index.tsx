ab11a58b2586d058c756ae1d6785f336b8535197
aishwarya more <aishsony88@gmail.com>
2025-03-01 01:06:47 +0530

    hiring logos changed

--- src/components/HiringPartnersAlt/index.tsx
+++ src/components/HiringPartnersAlt/index.tsx
@@ -12,52 +12,52 @@ import 'swiper/css/navigation';
 const HiringPartnersAlt = () => {
   const [activeTab, setActiveTab] = useState("global");
 
-  const banks = {
-    global: [
-      {
-        name: "J.P. Morgan",
-        logo: "https://cdn.prod.website-files.com/63f6e52346a353ca1752970e/644fb7a5f64fb5cb87a5beaa_20230501T1259-9f1793fc-a440-492c-a2b5-c01eba32c7f4.jpeg",
-      },
-      {
-        name: "Goldman Sachs",
-        logo: "https://worldgbc.s3.eu-west-2.amazonaws.com/wp-content/uploads/2022/08/08135440/GSsignature_Blue.png",
-      },
-      {
-        name: "Morgan Stanley",
-        logo: "https://pbs.twimg.com/profile_images/1631347869687898142/ATwo7QZZ_400x400.jpg",
-      },
-      {
-        name: "Bank of America",
-        logo: "https://logos-world.net/wp-content/uploads/2020/11/Bank-of-America-Emblem.png",
-      },
-      {
-        name: "Barclays",
-        logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaAMBzVwmCWxPtgjiI7i7X4vrHX7GcH9i0zQ&s",
-      },
-    ],
-    india: [
-      {
-        name: "Kotak Mahindra",
-        logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0x-5XTuch9fn5z1DFeFeNUI7iaWTAsN9Bpg&s",
-      },
-      {
-        name: "HDFC Bank",
-        logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsiMz-D2pGIen44gUk-84rgzGpeOzLuh3vgQ&s",
-      },
-      {
-        name: "BNP Paribas",
-        logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiLoiOe8E7v5GQnEVMFBQes0_igRqgSTiM4Q&s",
-      },
-      {
-        name: "ICICI Securities",
-        logo: "https://companieslogo.com/img/orig/ISEC.NS_BIG-46531085.png?t=1720452992",
-      },
-      {
-        name: "JM Financial",
-        logo: "https://signup.jmfonline.in/assets/images/JM%20Logo-%20Blue%20Logo.png",
-      },
-    ],
-  };
+  // const banks = {
+  //   global: [
+  //     {
+  //       name: "J.P. Morgan",
+  //       logo: "https://cdn.prod.website-files.com/63f6e52346a353ca1752970e/644fb7a5f64fb5cb87a5beaa_20230501T1259-9f1793fc-a440-492c-a2b5-c01eba32c7f4.jpeg",
+  //     },
+  //     {
+  //       name: "Goldman Sachs",
+  //       logo: "https://worldgbc.s3.eu-west-2.amazonaws.com/wp-content/uploads/2022/08/08135440/GSsignature_Blue.png",
+  //     },
+  //     {
+  //       name: "Morgan Stanley",
+  //       logo: "https://pbs.twimg.com/profile_images/1631347869687898142/ATwo7QZZ_400x400.jpg",
+  //     },
+  //     {
+  //       name: "Bank of America",
+  //       logo: "https://logos-world.net/wp-content/uploads/2020/11/Bank-of-America-Emblem.png",
+  //     },
+  //     {
+  //       name: "Barclays",
+  //       logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaAMBzVwmCWxPtgjiI7i7X4vrHX7GcH9i0zQ&s",
+  //     },
+  //   ],
+  //   india: [
+  //     {
+  //       name: "Kotak Mahindra",
+  //       logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0x-5XTuch9fn5z1DFeFeNUI7iaWTAsN9Bpg&s",
+  //     },
+  //     {
+  //       name: "HDFC Bank",
+  //       logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsiMz-D2pGIen44gUk-84rgzGpeOzLuh3vgQ&s",
+  //     },
+  //     {
+  //       name: "BNP Paribas",
+  //       logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiLoiOe8E7v5GQnEVMFBQes0_igRqgSTiM4Q&s",
+  //     },
+  //     {
+  //       name: "ICICI Securities",
+  //       logo: "https://companieslogo.com/img/orig/ISEC.NS_BIG-46531085.png?t=1720452992",
+  //     },
+  //     {
+  //       name: "JM Financial",
+  //       logo: "https://signup.jmfonline.in/assets/images/JM%20Logo-%20Blue%20Logo.png",
+  //     },
+  //   ],
+  // };
 
   const bank2 = [
     {
